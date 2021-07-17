bb89210478fcd82e5dc6d106bb44487dad8ef7e7
zxcv9203 <zxcv9203@naver.com>
2021-07-17 16:39:20 +0900

    yongckim 1weeks solved

---
+++ 1weeks/README.md
@@ -0,0 +1,4 @@
+# 1주차
+## yongckim 
+### 두 정수사이의 합 https://programmers.co.kr/learn/courses/30/lessons/12912
+두 정수 a, b가 주어졌을 때 a와 b사이 속한 모든 정수의 합을 리턴하는 함수를 완성하세요
---
+++ 1weeks/n_num.go
@@ -0,0 +1,12 @@
+package programers
+
+func solution(a int, b int) int64 {
+	ans := 0
+	if a > b {
+		a, b = b, a
+	}
+	for i := a; i <= b; i++ {
+		ans += i
+	}
+	return int64(ans)
+}
