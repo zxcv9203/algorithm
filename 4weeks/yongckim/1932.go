f7eed64b2f3ac7233d7986da8b3cff1c4dea09f1
kim <zxcv9203@naver.com>
2021-08-12 17:02:32 +0900

    1932 go solved

---
+++ 4weeks/yongckim/1932.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const M int = 501
+var dp[M][M] int
+var tri[M][M] int
+var n int
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
+func main() {
+	var ans int
+	r, w := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	fmt.Fscan(r, &n)
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Fscan(r, &tri[i][j])
+		}
+	}
+	dp[1][1] = tri[1][1]
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			if j == 1 {
+				dp[i][j] = dp[i - 1][j] + tri[i][j]
+			} else if j == i {
+				dp[i][j] = dp[i - 1][j - 1] + tri[i][j]
+			} else {
+				dp[i][j] = max(dp[i - 1][j - 1] + tri[i][j], dp[i - 1][j] + tri[i][j])
+			}
+		}
+	}
+	for i := 1; i <= n; i++ {
+		ans = max(ans, dp[n][i])
+	}
+	fmt.Println(ans)
+}
