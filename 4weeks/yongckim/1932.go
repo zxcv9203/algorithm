4df2763602306fa2645d019481863a36fbcebdfe
yongckim <zxcv9203@naver.com>
2021-08-13 02:51:48 +0900

    go 1932 refactoring

--- 4weeks/yongckim/1932.go
+++ 4weeks/yongckim/1932.go
@@ -30,13 +30,7 @@ func main() {
 	dp[1][1] = tri[1][1]
 	for i := 2; i <= n; i++ {
 		for j := 1; j <= i; j++ {
-			if j == 1 {
-				dp[i][j] = dp[i - 1][j] + tri[i][j]
-			} else if j == i {
-				dp[i][j] = dp[i - 1][j - 1] + tri[i][j]
-			} else {
-				dp[i][j] = max(dp[i - 1][j - 1] + tri[i][j], dp[i - 1][j] + tri[i][j])
-			}
+			dp[i][j] = tri[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j])
 		}
 	}
 	for i := 1; i <= n; i++ {
