713e432f75b3cab89e3b1f6f8ab3bc39818cc23c
kim <zxcv9203@naver.com>
2021-08-22 13:29:47 +0900

    17213 solved

--- 5weeks/yongckim/17213.go
+++ 5weeks/yongckim/17213.go
@@ -6,22 +6,17 @@ import (
 	"os"
 )
 
-var dp [31][31]int
+var dp [11][31]int
 var n, m int
-
-
 func main() {
 	r, w := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 	fmt.Fscan(r, &n, &m)
-	for i := 0; i < n; i++ {
-		dp[i][i] = 1
-	}
 	for i := 0; i < m; i++ {
 		dp[0][i] = 1
 	}
 	for i := 1; i < n; i++ {
-		for j := i + 1; j < m; j++ {
+		for j := i; j < m; j++ {
 			for k := 0; k < j; k++ {
 				dp[i][j] += dp[i - 1][k]
 			}
