74c981a876173f21c20475a656afdd60c44b9b4c
kim <zxcv9203@naver.com>
2021-08-24 03:15:19 +0900

    17204 solved

--- 6weeks/yongckim/17204.go
+++ 6weeks/yongckim/17204.go
@@ -8,15 +8,25 @@ import (
 
 var n, k int
 var a [150]int
-var visit [150]int
 
-func dfs(idx int) {
 
+func dfs(player, cnt int) {
+	if player == k {
+		fmt.Println(cnt)
+		return
+	} else if cnt == n {
+		fmt.Println(-1)
+		return
+	}
+	dfs(a[player], cnt + 1)
 }
 
 func main() {
 	r, w := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 	fmt.Fscan(r, &n, &k)
-	dfs(0)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &a[i])
+	}
+	dfs(0, 0)
 }
