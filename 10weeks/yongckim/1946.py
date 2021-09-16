31e0772e89d7fc798aaa97c189fd939d475c29f6
yongckim <zxcv9203@naver.com>
2021-09-16 22:52:34 +0900

    1946 solved

--- 10weeks/yongckim/1946.py
+++ 10weeks/yongckim/1946.py
@@ -1,5 +1,15 @@
+import sys
+input = sys.stdin.readline
 t = int(input())
 for i in range(t) :
 	n = int(input())
 	human = [list(map(int,input().split())) for i in range(n)]
+	human.sort()
+	cnt = 1
+	rank = human[0][1]
+	for i in range(1, n):
+		if rank > human[i][1]:
+			cnt += 1
+			rank = human[i][1]
+	print(cnt)
 	
