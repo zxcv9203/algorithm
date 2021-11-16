f9f5fea1587688a06afd6c365e92a3f9b79236cb
Yongcheol Kim <yongckim@c6r4s1.42seoul.kr>
2021-11-16 15:26:39 +0900

    16929 solved

--- 15 weeks/yongckim/16929.py
+++ 15 weeks/yongckim/16929.py
@@ -1,16 +1,31 @@
 import sys
+import itertools
 input = sys.stdin.readline
 
 CONST_M = 50
 
 ox = 0
 oy = 0
-ok = "No"
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
+aaa = "No"
+
 visit = []
 [visit.append([]) for x in range(CONST_M)]
 [visit[x].append(False) for x in range(CONST_M) for y in range(CONST_M)]
+
+def dfs(x, y, cnt):
+	visit[x][y] = True
+	for i in range(4):
+		nx = x + dx[i]
+		ny = y + dy[i]
+		if safe(nx, ny):
+			if cnt >= 4 and visit[nx][ny] == True and ox == nx and oy == ny:
+				global aaa
+				aaa = "Yes"
+				return
+			if visit[nx][ny] == False and dots[x][y] == dots[nx][ny]:
+				dfs(nx, ny, cnt + 1)
 
 n, m = [int(x) for x in input().split()]
 dots = []
@@ -20,23 +35,16 @@ for x in range(n):
 def safe(x, y):
 	return (x >= 0 and x < n and y >= 0 and y < m)
 
-def dfs(x, y, cnt):
-	visit[x][y] = True
-	for i in range(4):
-		nx = x + dx[i]
-		ny = y + dy[i]
-		if safe(nx, ny):
-			if ox == nx and oy == ny:
-				ok = "Yes"
-				return
-			if visit[nx][ny] == False and dots[x][y] == dots[nx][ny]:
-				dfs(nx, ny, cnt + 1)
+
 
 for x in range(n):
 	for y in range(m):
+		for k in range(CONST_M):
+			visit[k][:] = itertools.repeat(False, len(visit))
 		ox = x
 		oy = y
-		dfs(x, y, 0)
-		if ok == "Yes":
+		dfs(x, y, 1)
+		if aaa == "Yes":
 			break
-print(ok)
+
+print(aaa)
