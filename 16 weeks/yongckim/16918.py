90d9d5311ecafc27720e8ce80b7b68aab4dcfd36
yongckim <zxcv9203@naver.com>
2021-11-20 20:36:48 +0900

    16918 solved

--- 16 weeks/yongckim/16918.py
+++ 16 weeks/yongckim/16918.py
@@ -1,27 +1,43 @@
 import sys
-import queue
+from collections import deque
 input = sys.stdin.readline
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 r, c, n = [int(x) for x in input().split()]
-bomber = []
-[bomber.append(input().rstrip()) for x in range(r)]
-qx = queue.Queue()
-qy = queue.Queue()
+bomber = [list(input().rstrip()) for x in range(r)]
+dq = deque()
+def safe(x, y) :
+	return x >= 0 and x < r and y >= 0 and y < c
 
-def bomLocation():
+def bomSet():
 	for x in range(r):
 		for y in range(c):
 			if bomber[x][y] == 'O':
-				qx.put(x)
-				qy.put(y)
+				dq.append((x, y))
+			else :
+				bomber[x][y] = 'O'
 
 def bomPrint():
 	for x in range(r):
-		print(bomber[x])
+		for y in range(c):
+			print(bomber[x][y], end="")
+		print()
 
-bomLocation()
+def boom():
+	while dq:
+		x, y = dq.popleft()
+		bomber[x][y] = '.'
+		for i in range(4):
+			nx = x + dx[i]
+			ny = y + dy[i]
+			if safe(nx, ny) == True and bomber[nx][ny] == 'O':
+				bomber[nx][ny] = '.'
+i = 1
+while i <= n:
+	if i % 2 == 0:
+		bomSet()
+	else :
+		boom()
+	i += 1
 bomPrint()
-while qx.empty() == False and qy.empty() == False:
-	print(qx.get(), qy.get())
